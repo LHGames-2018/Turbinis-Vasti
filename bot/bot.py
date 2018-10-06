c5d098cfb848e252a57e485a70ee325ec1a35e10
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2018-10-06 13:26:31 -0400

    made array map of surroundings

--- bot/bot.py
+++ bot/bot.py
@@ -12,15 +12,32 @@ class Bot:
         """
         self.PlayerInfo = playerInfo
 
+    def surroundings(self, gameMap):
+        local = []
+        for y in range(gameMap.yMin, gameMap.yMax):
+            temp = []
+            for x in range(gameMap.xMin, gameMap.xMax):
+               # print(str(x) + " " + str(y))
+                temp.append(gameMap.getTileAt(Point(x, y)))
+            local.append(temp)
+        return local
+
     def execute_turn(self, gameMap, visiblePlayers):
         """
         This is where you decide what action to take.
             :param gameMap: The gamemap.
             :param visiblePlayers:  The list of visible players.
         """
-
+        print(gameMap)
+        print(gameMap.getTileAt(Point(0,1)))
+        print(gameMap.xMin)
+        print(gameMap.xMax)
+        print(gameMap.yMin)
+        print(gameMap.yMin)
+        print(self.surroundings(gameMap))
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
-        return create_move_action(Point(-1, 0))
+        return create_move_action(Point(1, 0))
+
 
     def after_turn(self):
         """
